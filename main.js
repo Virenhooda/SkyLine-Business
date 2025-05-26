3141466ce4e1ad2183bb1465da2414bf61cb3d02
Virenhooda <virenderofficial17@gmail.com>
2025-05-26 12:07:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAziLOw5DEFxUaNSEyOic0_FUf4ScLO5Ks",
   authDomain: "skyline-business.firebaseapp.com",
   projectId: "skyline-business",
-  storageBucket: "skyline-business.firebasestorage.app",
+  storageBucket: "skyline-business.appspot.com",
   messagingSenderId: "915397554137",
   appId: "1:915397554137:web:76316587fd556b72e0f3c8"
 };
