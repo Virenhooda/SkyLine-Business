0110d98237c4245cf9cfcebe9f5e2b5781a965ca
Virenhooda <virenderofficial17@gmail.com>
2025-05-26 11:17:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+import { initApp } from './main.js';
+initApp();
---
+++ main.js
@@ -0,0 +1,3 @@
+export function initApp() {
+    document.getElementById('app').innerHTML = '<h1>Welcome to SkyLine Business</h1>';
+}
